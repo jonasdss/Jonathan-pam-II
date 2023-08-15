60a86d166ef50baa5711bb98cfac9b854767ef24
john <js9056215@gmail.com>
2023-08-14 22:17:25 -0300

    variaveis e funcoes 14/08/

---
+++ ReactNative/IntroducaoAoJS/Aula01-Funcoes.js
@@ -0,0 +1,70 @@
+let n1 = 5;
+let n2 = 8;
+
+console.log(n1+n2);  //soma
+console.log(n1-n2);  //subtração
+console.log(n1*n2);  //multiplicação
+console.log(n1/n2);  //divisão
+console.log(n1**n2);  //expnenciação
+console.log(Math.sqrt(n1))  //raiz quadrada
+console.warn("Olá Funções")
+
+//soma
+function soma(numero1, numero2){
+    return numero1 + numero2
+}
+
+console.log(soma(4, 19))
+console.log(soma(12, 19))
+
+//subtração
+function subtração(numero1, numero2){
+    return numero1 - numero2
+}
+
+console.log(subtração(4, 19))
+console.log(subtração(12, 19))
+
+//multiplicação
+function multiplicação(numero1, numero2){
+    return numero1 * numero2
+}
+
+console.log(multiplicação(4, 19))
+console.log(multiplicação(12, 19))
+
+//divisão
+function divisão(numero1, numero2){
+    return numero1 / numero2
+}
+
+console.log(divisão(4, 19))
+console.log(divisão(12, 19))
+
+//exponenciação
+function expnenciação(numero1, numero2){
+    return numero1 ** numero2
+}
+
+console.log(expnenciação(4, 19))
+console.log(expnenciação(12, 19))
+
+//raiz quadrada
+function raiz(numero1, numero2){
+    return numero1 + numero2
+}
+
+console.log(raiz(4, 19))
+console.log(raiz(12, 19))
+
+const divisao = (v1, v2) =>{
+    if (v1 > v2) {
+        let resultado = v1 / v2
+        console.log(resultado) 
+    }
+    return "insira um valor para v2 menor"
+
+}
+
+divisao(10, 2)
+divisao(2, 10)
---
+++ ReactNative/IntroducaoAoJS/Aula01-Variaveis.js
@@ -0,0 +1,19 @@
+let variavelLet = "Olá"
+var variacelVar = 2
+const variavelConst = 3
+
+console.log(variavelLet)
+console.log(typeof variavelLet)
+console.log(variacelVar + variavelConst)
+variavelLet = 6 
+console.log(variacelVar + variavelLet)
+console.log(typeof variavelLet)
+variavelLet = "Olá Mundo"
+console.log(variavelLet + variacelVar)
+console.log(variavelLet, variacelVar)
+
+
+
+
+
+
