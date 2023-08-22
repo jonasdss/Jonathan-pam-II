00e153055459393885d4e18ef9a2129df89d0c41
john <js9056215@gmail.com>
2023-08-21 22:08:47 -0300

    aula 21/08

---
+++ ReactNative/IntroducaoAoJS/Aula2.js
@@ -0,0 +1,17 @@
+const obj = {
+    nome: "jonathan",
+    signo: "gemeos",
+    idade: 20,
+    louco: true,
+    parentes: ["rosangela", "Lourival", "calvinho"]
+}
+//Formato JSON - JavaScript Notation Object
+console.log(obj.nome, obj.signo)
+console.log(obj)
+console.log(typeof obj)
+console.log(obj.parentes[1])
+
+let pessoa = ["jonathan", 20, "DEV"]
+console.log(pessoa[0], pessoa[2])
+console.log(pessoa)
+console.log(typeof pessoa)
